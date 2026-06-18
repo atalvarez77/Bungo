aaa91281ee7b462e3966434c9677099b739d6c23
Adrian Alvarez <atalvarez77@gmail.com>
2026-06-18 09:45:35 +0900

    Updated requirements.txt, fixed venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/a2macpro/Desktop/Coding Projects/Bungo/venv"
+set -gx VIRTUAL_ENV "/Users/a2macpro/Desktop/Coding Projects/08 | Bungo/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
